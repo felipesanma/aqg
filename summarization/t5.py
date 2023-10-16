8f4b07ac208e9659ed24b7b49f954d4ef24abb78
felipesanma <felipesmsoto>
2023-10-16 12:09:54 -0300

    add: qgen init and summarization with t5

---
+++ summarization/t5.py
@@ -0,0 +1,85 @@
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+import random
+import numpy as np
+import nltk
+
+nltk.download("punkt")
+nltk.download("brown")
+nltk.download("wordnet")
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import sent_tokenize
+
+
+summary_model = T5ForConditionalGeneration.from_pretrained("t5-base")
+summary_tokenizer = T5Tokenizer.from_pretrained("t5-base")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+summary_model = summary_model.to(device)
+
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+set_seed(42)
+
+
+def postprocesstext(content):
+    final = ""
+    for sent in sent_tokenize(content):
+        sent = sent.capitalize()
+        final = final + " " + sent
+    return final
+
+
+def summarizer(text, model, tokenizer):
+    text = text.strip().replace("\n", " ")
+    text = "summarize: " + text
+    # print (text)
+    max_len = 512
+    encoding = tokenizer.encode_plus(
+        text,
+        max_length=max_len,
+        pad_to_max_length=False,
+        truncation=True,
+        return_tensors="pt",
+    ).to(device)
+
+    input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
+
+    outs = model.generate(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        early_stopping=True,
+        num_beams=3,
+        num_return_sequences=1,
+        no_repeat_ngram_size=2,
+        min_length=75,
+        max_length=300,
+    )
+
+    dec = [tokenizer.decode(ids, skip_special_tokens=True) for ids in outs]
+    summary = dec[0]
+    summary = postprocesstext(summary)
+    summary = summary.strip()
+
+    return summary
+
+
+"""
+text = "There is a lot of volcanic activity at divergent plate boundaries in the oceans. For example, many undersea volcanoes are found along the Mid-Atlantic Ridge. This is a divergent plate boundary that runs north-south through the middle of the Atlantic Ocean. As tectonic plates pull away from each other at a divergent plate boundary, they create deep fissures, or cracks, in the crust. Molten rock, called magma, erupts through these cracks onto Earth’s surface. At the surface, the molten rock is called lava. It cools and hardens, forming rock. Divergent plate boundaries also occur in the continental crust. Volcanoes form at these boundaries, but less often than in ocean crust. That’s because continental crust is thicker than oceanic crust. This makes it more difficult for molten rock to push up through the crust. Many volcanoes form along convergent plate boundaries where one tectonic plate is pulled down beneath another at a subduction zone. The leading edge of the plate melts as it is pulled into the mantle, forming magma that erupts as volcanoes. When a line of volcanoes forms along a subduction zone, they make up a volcanic arc. The edges of the Pacific plate are long subduction zones lined with volcanoes. This is why the Pacific rim is called the “Pacific Ring of Fire.”"
+
+summarized_text = summarizer(text, summary_model, summary_tokenizer)
+
+print("\noriginal Text >>")
+print(text)
+print("\n")
+print("Summarized Text >>")
+print(summarized_text)
+print("\n")
+"""
