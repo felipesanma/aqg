fedd43e274200a53a7a688cbdb87f4e20271f02b
felipesanma <felipesmsoto>
2023-10-17 23:45:17 -0300

    add: better random chunks method

--- pyqgen/content_processing/splitter.py
+++ pyqgen/content_processing/splitter.py
@@ -99,12 +99,17 @@ class ContentSplitter:
             chunks.remove("")
         return chunks
 
-    def get_random_chunks(self, *, chunks: list, n_choices: int = 5):
+    def get_random_chunks(
+        self, *, chunks: list, n_choices: int = 5, n_characters: int = 2000
+    ):
         choices = []
-
-        for i in range(n_choices):
+        while len(choices) < n_choices:
             sample = random.choice(chunks)
-            if sample not in choices:
+            if sample not in choices and len(sample) >= n_characters:
                 choices.append(sample)
-
         return choices
+
+    def get_largest_chunks(self, *, chunks: list, n_choices: int = 5):
+        sorted_list = sorted(chunks, key=len, reverse=True)
+
+        return sorted_list[:n_choices]
--- test.py
+++ test.py
@@ -11,11 +11,25 @@ content = pyqgen.pdf_to_text.extract(pdf_file=files[0])
 chunks_text = pyqgen.splitter.generate_chunks_text(content=content)
 
 choices = 5
-chunks_choices = pyqgen.splitter.get_random_chunks(
+random_chunks_choices = pyqgen.splitter.get_random_chunks(
     chunks=chunks_text, n_choices=choices
 )
 
-for i, chunk in enumerate(chunks_choices):
-    print(len(chunk))
 
-print(len(chunks_choices))
+def log_chunks(chunks: list):
+    print(" ")
+    for chunk in chunks:
+        print(" ")
+        print(len(chunk))
+        print(" ")
+        print(chunk)
+    print(" ")
+    print(len(chunks))
+    print(" ")
+
+
+log_chunks(random_chunks_choices)
+
+largest_chunks = pyqgen.splitter.get_largest_chunks(chunks=chunks_text)
+
+log_chunks(largest_chunks)
