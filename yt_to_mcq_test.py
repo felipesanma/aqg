06dafef646eaff1421cb566d3d8c5fb1d1ac31d1
felipesanma <felipesmsoto>
2023-10-26 15:33:36 -0300

    add: test for generating mcq about a youtube video

--- yt_to_mcq_test.py
+++ yt_to_mcq_test.py
@@ -6,3 +6,8 @@ video_id = "CZn236ZHw6A"
 
 video_info = pyqgen.youtube_video_to_text.extract(video_id=video_id)
 print(video_info)
+print(len(video_info["timestamp_and_content_mapping"]))
+for content in video_info["timestamp_and_content_mapping"]:
+    print(content)
+    mcq_questions = pyqgen.mcq.generate_mcq_questions(content=content["content"])
+    print(mcq_questions)
