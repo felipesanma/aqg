56c18eaaae6adcbdfab6922aa406fc946e7650e2
felipesanma <felipesmsoto>
2023-10-31 19:25:08 -0300

    add: refactoring language detection support en es

--- pyqgen/content_processing/language_detection.py
+++ pyqgen/content_processing/language_detection.py
@@ -1,13 +1,23 @@
 from lingua import Language, LanguageDetectorBuilder
 
-languages = [Language.ENGLISH, Language.FRENCH, Language.GERMAN, Language.SPANISH]
 
-detector = LanguageDetectorBuilder.from_languages(*languages).build()
+LANGUAGES = [Language.ENGLISH, Language.SPANISH]
+LANGUAGES_MAPPING = {
+    "SPANISH": {"prompt": "Spanish", "language_code": "es"},
+    "ENGLISH": {"prompt": "English", "language_code": "en"},
+}
+
+
+def detect_language_from_text(text: str, languages=LANGUAGES):
+    detector = LanguageDetectorBuilder.from_languages(*languages).build()
+    language = detector.detect_language_of(text)
+    return LANGUAGES_MAPPING[language.name]
+
 
 long_text = """ se oye bien como no me permites cambiar el tema de mi tesis renuncio al proyecto de concluirla y defenderla soy consciente de que al renunciar de este modo unilateral perderé la beca que ahora disfruto Pero lo prefiero así con tal de liberarme de la servidumbre intelectual a la que me ha sometido Durante los últimos dos años dos años confirmando artículos que he escrito yo solita dos años impartiendo más docencia de la que me toca sobre asignaturas que ignoro por completo y a alumnos que parecen retrasado
 """
-short_text = "Hola, qué tal? Me gustaría invitarte un café"
-confidence_values = detector.compute_language_confidence_values(short_text)
+short_text = ""
 
-print(detector.detect_language_of(short_text))
-print(confidence_values[0].language)
+language = detect_language_from_text(short_text)
+
+print(language)
